d7e3642c36fdab2a196e459c5d9e4cf735d94870
Erfan5555 <109932909+Erfan5555@users.noreply.github.com>
2023-04-08 22:56:32 +0930

    JS postig functionality, posts get appended upwareds, the buttons work properly with the insert box getting hidden when not required

--- Forum.js
+++ Forum.js
@@ -2,12 +2,28 @@
 const PostButton= document.getElementById('PostButton');
 const Forum = document.querySelector("#BackGround")
 postBox= document.querySelector('#post');
+SubmitPost= document.getElementById('postButton');
 const TextToInsert = document.getElementById("y");
+var click;
 
 PostButton.addEventListener('click',function(){
+    TextToInsert.style.display='block'
+    SubmitPost.style.display='block'
+    PostButton.style.display='none'
     
-    var Text= TextToInsert.value;
-    const newPost= postBox.cloneNode(true);
-    newPost.querySelector('p').innerHTML=Text;
-    Forum.appendChild(newPost);
 })
+
+
+SubmitPost.addEventListener('click',function(){
+     
+            var Text= TextToInsert.value;
+            const newPost= postBox.cloneNode(true);
+            newPost.querySelector('p').innerHTML=Text;
+            Forum.insertBefore(newPost,postBox);
+           
+            TextToInsert.style.display='block'
+        
+      
+    })
+
+
