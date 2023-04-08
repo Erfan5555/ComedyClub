0f7dd010f21eae1540cf24a463c2c66b5c27aa6c
Erfan5555 <109932909+Erfan5555@users.noreply.github.com>
2023-04-08 22:17:36 +0930

    added JS functionality for posting
    --all

--- Forum.js
+++ Forum.js
@@ -1,1 +1,13 @@
 
+const PostButton= document.getElementById('PostButton');
+const Forum = document.querySelector("#BackGround")
+postBox= document.querySelector('#post');
+const TextToInsert = document.getElementById("y");
+
+PostButton.addEventListener('click',function(){
+    
+    var Text= TextToInsert.value;
+    const newPost= postBox.cloneNode(true);
+    newPost.querySelector('p').innerHTML=Text;
+    Forum.appendChild(newPost);
+})
